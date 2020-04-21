fc584564c8eca9d01e0efdad2ef6b31c9d29a3ae
Maciek Bednarski <mBednar2020@gmail.com>
2020-04-21 19:21:29 +0200

    added navbar, basic page design

--- BookExchange-Front/src/app/app.module.ts
+++ BookExchange-Front/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, BookComponent],
+  declarations: [AppComponent, BookComponent, NavbarComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
