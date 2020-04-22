a1e194220af8efa699c277a5671eb31cdfe96c6d
Maciek Bednarski <mBednar2020@gmail.com>
2020-04-22 02:17:00 +0200

    Added logging in frontend

--- BookExchange-Front/src/app/app.module.ts
+++ BookExchange-Front/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
   declarations: [AppComponent, BookComponent, NavbarComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
